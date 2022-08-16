58748ad04cf739c281ebe41c812fe96ea00792dd
TomB <44700588+drtbz@users.noreply.github.com>
2022-08-16 21:02:59 +0100

    cleanup

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			log.Printf("%v: {Chart: %v, App: %v} \n", re["name"], re["version"], re["app_version"])	
+			log.Printf("%v: Chart: %v, App: %v \n", re["name"], re["version"], re["app_version"])	
 
 		}
 	}
@@ -55,4 +55,32 @@ func GitHubSetup() (context.Context, *github.Client) {
 func LatestRelease(client *github.Client, ctx context.Context, owner, repo string) (release *github.RepositoryRelease, resp *github.Response, err error) {
 	release, resp, err = client.Repositories.GetLatestRelease(ctx, owner, repo)
 	return
+}
+
+func ListReleases(client *github.Client, ctx context.Context, owner, repo string, prerelease ...bool, ) {
+	opt := &github.ListOptions{
+		Page:    0,
+		PerPage: 50,
+	}
+	var allRelease []*github.RepositoryRelease
+	for {
+		repos, resp, err := client.Repositories.ListReleases(ctx, owner, repo, opt)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+		allRelease = append(allRelease, repos...)
+		log.Printf("FirstPage: %v ", resp.FirstPage)
+		log.Printf("LastPage: %v ", resp.LastPage)
+		log.Printf("NextPage: %v \n ", resp.NextPage)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	for _, v := range allRelease {
+		if !*v.Prerelease {
+			log.Printf("%v\n", github.Stringify(v.Name))
+		}
+	}
 }
